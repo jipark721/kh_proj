d511c6bc45ffc41072ccaa7edd5e5468c1935d92
Alex Ahn <alexahn917@gmail.com>
2017-06-03 23:20:45 +0900

    print testing

--- mongodb/test_db.py
+++ mongodb/test_db.py
@@ -1,8 +1,15 @@
 from pymongo import MongoClient
+from pprint import pprint
+
 client = MongoClient()
 
 print("Printing mongodb configurations...\n")
 print("Databases:\n%s\n" %client.database_names())
 print("Collections under khdb:\n%s\n" %client.get_database("khdb").collection_names())
 
+khdb = client.get_database("khdb")
 
+patients = khdb.get_collection("patients")
+
+for patient in patients.find():
+    pprint(patient)
