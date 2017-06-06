fffc8cb25f0a7611af1daba79daf626827356830
Alex Ahn <alexahn917@gmail.com>
2017-06-07 01:12:36 +0900

    parsing patients done

--- mongodb/test_db.py
+++ mongodb/test_db.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from pymongo import MongoClient
 from pprint import pprint
+from utils import *
+
+reset_database()
+initialize_database()
 
 client = MongoClient()
 
@@ -7,11 +13,32 @@ print("Printing mongodb configurations...\n")
 print("Databases:\n%s\n" % client.database_names())
 print("Collections under khdb:\n%s\n" % client.get_database("khdb").collection_names())
 
-khdb = client.get_database("khdb")
-patients = khdb.get_collection("patients")
-diseases = khdb.get_collection("diseases")
-ingredients = khdb.get_collection("ingredients")
+client = MongoClient()
+khdb = client.khdb
+patients = khdb.patients
+diseases = khdb.diseases
+ingredients = khdb.ingredients
+nutrients = khdb.nutrients
 
 print("There are %d many patients data" % patients.count())
 print("There are %d many diseases data" % diseases.count())
 print("There are %d many ingredients data\n" % ingredients.count())
+
+
+def print_all_patients():
+    for patient in patients.find():
+        pprint(patient["알레르기음식"])
+
+def print_all_diseases():
+    for disease in diseases.find():
+        pprint(disease["질병명"])
+
+def print_all_ingredients():
+    for ingredient in ingredients.find():
+        pprint(ingredient["식품명"])
+
+def print_all_nutrients():
+    for nutrient in nutrients.find():
+        pprint(nutrient["영양소명"])
+
+print_all_patients()
--- mongodb/utils.py
+++ mongodb/utils.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-
 from pymongo import MongoClient
+from pprint import pprint
 import json
 
 client = MongoClient()
 khdb = client.khdb
-patients = khdb.patients
-diseases = khdb.diseases
-ingredients = khdb.ingredients
+patients_collection = khdb.patients
+diseases_collection = khdb.diseases
+ingredients_collection = khdb.ingredients
 
 
 def reset_database():
@@ -16,49 +16,65 @@ def reset_database():
     khdb.drop_collection("ingredients")
 
 
-def initialize_database_from_json():
-    patients = json.load("json/patients")
-    for patient in patients:
-        print(patient)
+def initialize_database():
+    with open("json/patients.json") as patient_data:
+        patients = json.load(patient_data)
+        for patient in patients:
+            patient["알레르기음식"] = convert_list_2_tuple(patient["알레르기음식"])
+            add_one_patient(patient)
 
 
 def get_all_patients():
-    return patients.find()
+    return patients_collection.find()
 
 
 def get_patient_by_name(name):
-    return patients.find({"성명": name})
+    return patients_collection.find({"성명": name})
 
 
 def get_patient_by_id(id):
-    return patients.find({"ID": id})
+    return patients_collection.find({"ID": id})
 
 
 def get_patient_by_name_and_birthdate(name, birthdate):
-    return patients.find(
+    return patients_collection.find(
         {"$and":
              [{"성명": name},
               {"생일": birthdate}]
          }
     )
 
-def addOnePatient(id, name, sex, birthdate, address, height, weight, isPreg, isBFeeding, officeVisitDateList, diagDiseases):
-    return patients.insert_one(
+def add_one_patient(patient):
+    return patients_collection.insert_one(
         {
-            "ID": id,
-            "성명": name,
-            "성별": sex,
-            "생년월일": birthdate,
-            "주소": address,
-            "키": height,
-            "몸무게":weight,
-            "임신여부": isPreg,
-            "수유여부": isBFeeding,
-            "진료일": officeVisitDateList,
-            "진단명": diagDiseases
+        #"Num": 1.0,
+        "ID": patient['ID'],
+        "이름": patient['이름'],
+        "성별": patient['성별'],
+        "생년월일": patient['생년월일'],
+        "주소": patient['주소'],
+        "진단명": patient['진단명'],
+        "진료일": patient['진료일'],
+        "방문횟수": patient['방문횟수'],
+        "키": patient['키'],
+        "몸무게": patient['몸무게'],
+        "임신여부": patient['임신여부'],
+        "수유여부": patient['수유여부'],
+        "알레르기음식": patient["알레르기음식"]
         }
     )
 
+# Convert list of entry-defined (val_1, val_2) objects to list of tuples
+def convert_list_2_tuple(entry_list):
+    if entry_list == "" or entry_list is None:
+        return None
+    entries = [entry.strip() for entry in entry_list.split(",")]
+    return [convert_entry_2_tuple(tuple) for tuple in entries]
 
-
-pprint([p for p in getPatientByName("John Lex")])
+# (val_1:val_2) -> tup(val_1,val_2)
+def convert_entry_2_tuple(entry):
+    if entry == "" or entry is None:
+        return None
+    entry = entry.strip()[1:-1]
+    name, value = entry.split(":")
+    return tuple([name, int(value)])
