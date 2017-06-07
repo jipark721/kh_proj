d04463ba1e538504ae7d43a0bfb07394cb46c9fc
Alex Ahn <alexahn917@gmail.com>
2017-06-08 00:06:50 +0900

    changed readme and instructions

--- mongodb/init_db.sh
+++ mongodb/init_db.sh
@@ -1,5 +1,2 @@
+cp ~/Downloads/*.xlsx xls/.
 python xls2json.py
-mongoimport --db khdb --collection diseases --drop --file json/diseases.json --jsonArray
-mongoimport --db khdb --collection patients --drop --file json/patients.json --jsonArray
-mongoimport --db khdb --collection ingredients --drop --file json/ingredients.json --jsonArray
-mongoimport --db khdb --collection nutrients --drop --file json/nutrients.json --jsonArray
